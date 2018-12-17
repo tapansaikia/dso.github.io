030d414439cf49aac389aee6c010d62cde2db227
Tapan Saikia <37594259+tapansaikia@users.noreply.github.com>
2018-12-17 07:06:06 +0530

    Add files via upload

---
+++ dso.js
@@ -0,0 +1,25 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("myHeader").className = "Opacity";
+    } else {
+        document.getElementById("myHeader").className = "";
+    }
+}
+
+// slideshow code
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 3000); // Change image every 2 seconds
+}
